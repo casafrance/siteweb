f33ec1e882c5b3c42fd8e26e90a058d4d3616e8b
casafrance <casafrancia33000@gmail.com>
2019-02-25 03:12:59 +0100

    index to php

--- isolationComblesDevis.php
+++ isolationComblesDevis.php
@@ -270,7 +270,7 @@
 
     <div class="section"><span>2</span>Planification des travaux</div>
     <div class="inner-wrap">
-    	<label><b>Choisissez vos disponibilités pour le passage du technicien metreur  </b><input type="date" name="datemetrage" required="required"/><br><input type="time" name="heuremetrage" value="11:00" /></label>
+    	<label><b>Choisissez vos disponibilités pour le passage du metreur  </b><input type="date" name="datemetrage" required="required"/><br><input type="time" name="heuremetrage" value="11:00" /></label>
         <label><b>Indiquez vos disponibilité pour la date des travaux </b><input type="date" name="datedemarragetravaux" required="required" /></label>
     </div>
     <div class="section"><span>3</span>Vos coordonnées (créez votre compte)</div>
